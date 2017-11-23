1aa8f20d58cdf2e12f12ab9770e84b66a3dccf5f
Lion Pierau <lion.pierau@gmail.com>
2017-11-23 15:16:52 +0100

    added basic functions

---
+++ src/main/java/vsp/adventurer_api/FacadeController.java
@@ -0,0 +1,12 @@
+package vsp.adventurer_api;
+
+import static spark.Spark.get;
+
+public enum FacadeController {
+    Singleton;
+
+    public void run() {
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+}
