d0f6ab30699e3cf7d23007d77281f15449b123fe
Lion Pierau <lion.pierau@gmail.com>
2017-11-23 16:11:39 +0100

    added registration and login

--- src/main/java/vsp/Application.java
+++ src/main/java/vsp/Application.java
@@ -1,15 +1,25 @@
 package vsp;
 
 import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import vsp.adventurer_api.APIClient;
 import vsp.adventurer_api.FacadeController;
+import vsp.adventurer_api.entities.Token;
+import vsp.adventurer_api.entities.User;
+import vsp.adventurer_api.http.HTTPConnectionException;
+import vsp.adventurer_api.http.HTTPResponse;
+import vsp.adventurer_api.utility.BlackBoard;
 
+import java.io.Console;
 import java.io.IOException;
 
 /**
  * Runs application and interactions.
  */
 public class Application {
+    private static final int BLACKBOARD_PORT = 24000;
     private static Logger LOG = Logger.getLogger(Application.class);
+    private static Console terminal;
 
     /**
      * Holds only the main method an instance is not necessary.
@@ -19,11 +29,57 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
         LOG.debug("Starting application...");
-        // TODO register or login
+
+        BlackBoard blackBoard = new BlackBoard(BLACKBOARD_PORT);
+
+        try {
+            APIClient client = new APIClient(blackBoard.getHostAddress(), blackBoard.getPort());
+            // interactions
+            User user = insertUser();
+            LOG.debug("New user " + user.getName() + ":" + user.getPassword());
+            handleRegisterIfNecessary(client, user);
+        } catch (final IOException e) {
+            LOG.error(e);
+        }
 
         // TODO create adventurer in the taverna
 
         // Start rest-api
         FacadeController.Singleton.run();
     }
+
+    @NotNull
+    private static User insertUser() {
+        print("Please login or register with a username and password!");
+        terminal = System.console();
+        String username = terminal.readLine("Username: ");
+        String password = terminal.readLine("password: ");
+        return new User(username, password);
+    }
+
+    private static void handleRegisterIfNecessary(@NotNull final APIClient client,
+                                                  @NotNull final User user) throws IOException {
+        try {
+            final HTTPResponse response = client.register(user);
+            LOG.debug(response);
+            print("User is now registered!");
+        } catch (final IOException e) {
+            if (e instanceof HTTPConnectionException && ((HTTPConnectionException) e).getErrorCode() == 409)
+                print("User already registered!");
+            else
+                LOG.error(e.getMessage());
+        }
+
+        // login
+        final Token token = client.login(user).getAs(Token.class);
+        user.setToken(token);
+        print((token == null) ? "Login failed!" : "User logged in");
+        client.saveToken("default", token.getToken()); // TODO NPE
+    }
+
+    private static void print(@NotNull String message) {
+        System.out.println(message);
+    }
+
+
 }
---
+++ src/main/java/vsp/adventurer_api/APIClient.java
@@ -0,0 +1,140 @@
+package vsp.adventurer_api;
+
+
+import com.google.common.base.Preconditions;
+import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import vsp.Application;
+import vsp.TokenNotFoundException;
+import vsp.adventurer_api.entities.Token;
+import vsp.adventurer_api.entities.User;
+import vsp.adventurer_api.http.HTTPRequest;
+import vsp.adventurer_api.http.HTTPResponse;
+import vsp.adventurer_api.http.HTTPVerb;
+import vsp.adventurer_api.http.auth.HTTPBasicAuth;
+import vsp.adventurer_api.http.auth.HTTPTokenAuth;
+import vsp.adventurer_api.http.web_resource.DebugResource;
+import vsp.adventurer_api.http.web_resource.MainResource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Offers operations from the given API.
+ */
+public class APIClient {
+
+    private static Logger LOG = Logger.getLogger(Application.class);
+
+    @NotNull
+    private static final String PROTOCOL = "http";
+
+    @NotNull
+    private String targetURL;
+
+
+    @NotNull
+    private Map<String, String> tokenMap = new HashMap<>();
+
+    public APIClient(@NotNull final String restApiAddress,
+                     @NotNull final Integer restApiPort) {
+        Preconditions.checkNotNull(restApiAddress, "restApiAddress should not be null.");
+        Preconditions.checkNotNull(restApiPort, "restApiPort should not be null.");
+
+        this.targetURL = String.format("%s://%s:%d", PROTOCOL, restApiAddress, restApiPort);
+        LOG.debug("URL: " + targetURL);
+    }
+
+    // ======= for debug/testing ======
+
+    public HTTPResponse get(@NotNull final User user,
+                            @NotNull final String path) throws IOException {
+        LOG.debug("Registration with user " + user.getName());
+        return HTTPRequest
+                .to(targetURL)
+                .resource(new DebugResource(path))
+                .type(HTTPVerb.GET)
+                .auth(HTTPTokenAuth.forUser(user))
+                .send();
+    }
+
+    public HTTPResponse post(@NotNull final User user,
+                             @NotNull final String path,
+                             @NotNull final String body) throws IOException {
+        LOG.debug("Registration with user " + user.getName());
+        return HTTPRequest
+                .to(targetURL)
+                .resource(new DebugResource(path))
+                .type(HTTPVerb.POST)
+                .auth(HTTPTokenAuth.forUser(user))
+                .body(body)
+                .send();
+    }
+
+    public HTTPResponse put(@NotNull final User user,
+                            @NotNull final String path,
+                            @NotNull final String body) throws IOException {
+        LOG.debug("Registration with user " + user.getName());
+        return HTTPRequest
+                .to(targetURL)
+                .resource(new DebugResource(path))
+                .type(HTTPVerb.PUT)
+                .auth(HTTPTokenAuth.forUser(user))
+                .body(body)
+                .send();
+    }
+
+    //  =====================================
+
+    /**
+     * Registers a user to the blackboard.
+     *
+     * @param user Not null.
+     * @return TODO
+     * @throws IOException If connection fails.
+     */
+    public HTTPResponse register(@NotNull final User user) throws IOException {
+        LOG.debug("Registration with user " + user.getName());
+        return HTTPRequest
+                .to(targetURL)
+                .resource(MainResource.USERS)
+                .type(HTTPVerb.POST)
+                .body(user)
+                .send();
+    }
+
+    /**
+     * Login to receive a authentication token.
+     *
+     * @param user Not null.
+     * @return TODO
+     * @throws IOException If connection fails.
+     */
+    public HTTPResponse login(@NotNull final User user) throws IOException {
+        LOG.debug("Login with user '" + user.getName() + "'...");
+        return HTTPRequest
+                .to(targetURL)
+                .resource(MainResource.LOGIN)
+                .type(HTTPVerb.GET)
+                .auth(HTTPBasicAuth.forUser(user))
+                .send();
+    }
+
+    public void saveToken(String name, String token) {
+        tokenMap.put(name, token);
+        LOG.debug("Saved token " + name + "!");
+    }
+
+    @NotNull
+    public Map<String, String> getTokenMap() {
+        return tokenMap;
+    }
+
+    public Token getToken(String key) throws TokenNotFoundException {
+        String token = tokenMap.get(key);
+        if (token == null)
+            throw new TokenNotFoundException();
+        return new Token(token, null);
+    }
+}
