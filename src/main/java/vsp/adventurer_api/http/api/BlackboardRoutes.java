a31e86a3f02b158bc4293ff526b2610b536414d4
Lion Pierau <lion.pierau@gmail.com>
2017-12-05 17:12:00 +0100

    BUGFIX wrong quests route

--- src/main/java/vsp/adventurer_api/http/api/BlackboardRoutes.java
+++ src/main/java/vsp/adventurer_api/http/api/BlackboardRoutes.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
  * Offers all possible resources from the rest api.
  */
 public enum BlackboardRoutes implements Route {
-    ADVENTURERS("/taverna/adventurers"), USERS("/users"), LOGIN("/login"), WHOAMI("/whoami"), QUESTS("/quests"), MAP("/map"), GROUP("/taverna/groups"), VISITS("/visits");
+    ADVENTURERS("/taverna/adventurers"), USERS("/users"), LOGIN("/login"), WHOAMI("/whoami"), QUESTS("/blackboard/quests"), MAP("/map"), GROUP("/taverna/groups"), VISITS("/visits");
 
     @NotNull
     private String path;
